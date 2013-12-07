807c2b67d8922c37291fb3e18a77cd29159515e7
ase34 <asehrm34@gmail.com>
2013-12-07 18:53:03 +0100

    Adds hashCode, equals, toString

--- src/main/java/de/ase34/auqariums/Aquarium.java
+++ src/main/java/de/ase34/auqariums/Aquarium.java
@@ -60,4 +60,55 @@ public class Aquarium {
         }
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((fishes == null) ? 0 : fishes.hashCode());
+        result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+        result = prime * result + ((rootBlock == null) ? 0 : rootBlock.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Aquarium)) {
+            return false;
+        }
+        Aquarium other = (Aquarium) obj;
+        if (fishes == null) {
+            if (other.fishes != null) {
+                return false;
+            }
+        } else if (!fishes.equals(other.fishes)) {
+            return false;
+        }
+        if (owner == null) {
+            if (other.owner != null) {
+                return false;
+            }
+        } else if (!owner.equals(other.owner)) {
+            return false;
+        }
+        if (rootBlock == null) {
+            if (other.rootBlock != null) {
+                return false;
+            }
+        } else if (!rootBlock.equals(other.rootBlock)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Aquarium [rootBlock=" + rootBlock + ", owner=" + owner + ", fishes=" + fishes + "]";
+    }
+
 }
