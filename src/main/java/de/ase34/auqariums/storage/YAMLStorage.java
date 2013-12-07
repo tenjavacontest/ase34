e9a1d54edca102b5b09bcef5a6bc1d13b80d4927
ase34 <asehrm34@gmail.com>
2013-12-07 19:22:47 +0100

    Second storage code

--- src/main/java/de/ase34/auqariums/storage/YAMLStorage.java
+++ src/main/java/de/ase34/auqariums/storage/YAMLStorage.java
@@ -1,6 +1,9 @@
 package de.ase34.auqariums.storage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -50,8 +53,31 @@ public class YAMLStorage implements AbstractStorage {
 
     @Override
     public void save(Set<Aquarium> aquariums) {
-        // TODO Auto-generated method stub
+        List<HashMap<String, Object>> sections = new ArrayList<HashMap<String, Object>>();
 
+        for (Aquarium aquarium : aquariums) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+
+            map.put("owner", aquarium.getOwner());
+
+            ArrayList<Object> fishes = new ArrayList<Object>();
+            for (Fish fish : aquarium.getFishes()) {
+                fishes.add(fish.getType().toString());
+            }
+            map.put("fishes", fishes);
+
+            HashMap<String, Object> location = new HashMap<String, Object>();
+            location.put("world", aquarium.getRootBlock().getWorld().getName());
+            location.put("x", aquarium.getRootBlock().getX());
+            location.put("y", aquarium.getRootBlock().getY());
+            location.put("z", aquarium.getRootBlock().getZ());
+
+            map.put("rootBlock", location);
+
+            sections.add(map);
+        }
+
+        config.set("aquariums", sections);
     }
 
 }
